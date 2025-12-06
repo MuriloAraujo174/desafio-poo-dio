73bb249354cc05194967e773a37b7159a86308ac
Antonio Murilo <antoniomurilog@gmail.com>
2025-12-06 13:59:22 -0300

    Adição das clas mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Curso curso1 = new Curso();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Intermediario");
+        curso1.setCargaHoraria(52);
+
+        System.out.println(curso1);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Java");
+        mentoria.setDescricao("BootCamp Java");
+        mentoria.setDate(LocalDate.now());
+
+        System.out.println(mentoria);
+
     }
 }
