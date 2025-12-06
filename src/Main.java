fbde3be29a74ecdc4b681b2ff3622fcd4dd56dea
Antonio Murilo <antoniomurilog@gmail.com>
2025-12-06 13:59:44 -0300

    Adição das clas mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
